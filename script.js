4c441fc6a849b70c95c9a68892fa3f77071b75a3
Ashish Dattatraya Latake <95896308+webwithashish@users.noreply.github.com>
2023-02-24 17:26:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const btn = document.querySelector(".btn-country");
+// const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 let test;
 const btnSearch = document.querySelector(".search");
@@ -63,35 +63,6 @@ const renderCountry = function (data) {
     // countriesContainer.style.opacity = 1;
 };
 
-/////////////////////////////////////////////
-/* 
-const getInfoCountry = function (country) {
-    const request = new XMLHttpRequest();
-    request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-    request.send();
-
-    request.addEventListener("load", function () {
-        const data = JSON.parse(this.responseText);
-        test = data;
-        console.log(data);
-        data.forEach((ele) => {
-            renderCountry(ele);
-        });
-        // renderCountry(data);
-    });
-};
-// getInfoCountry("india");
-*/
-////////////////////////////////////////////////////////////
-// promises
-// const getInfoCountry = function (country) {
-//     fetch(`https://restcountries.com/v3.1/name/${country}`).then((Response) =>
-//         Response.json().then((data) =>
-//             data.forEach((data) => renderCountry(data))
-//         )
-//     );
-// };
-
 /////////////////////////////////////
 const getInfoCountry = function (country) {
     fetch(`https://restcountries.com/v3.1/name/${country}`)
@@ -109,103 +80,35 @@ const getInfoCountry = function (country) {
         })
         .finally((countriesContainer.style.opacity = 1));
 };
-// btn.addEventListener("click", function () {
-// getInfoCountry("india");
-// });
-// getInfoCountry("asdfaj");
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// CHALLENGE NO 1
-/* 
-// In this challenge you will build a function 'whereAmI' which renders a country
-// only based on GPS coordinates. For that, you will use a second API to geocode
-// coordinates. So in this challenge, you’ll use an API on your own for the first time �
-
-// Your tasks:
-// PART 1
-// 1. Create a function 'whereAmI' which takes as inputs a latitude value ('lat')
-// and a longitude value ('lng') (these are GPS coordinates, examples are in test
-// data below).
-
-// 2. Do “reverse geocoding” of the provided coordinates. Reverse geocoding means
-// to convert coordinates to a meaningful location, like a city and country name.
-// Use this API to do reverse geocoding: https://geocode.xyz/api. The AJAX call
-// will be done to a URL with this format:
-// https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and
-// promises to get the data. Do not use the 'getJSON' function we created, that
-// is cheating �
-
-// 3. Once you have the data, take a look at it in the console to see all the attributes
-// that you received about the provided location. Then, using this data, log a
-// message like this to the console: “You are in Berlin, Germany”
-
-// 4. Chain a .catch method to the end of the promise chain and log errors to the
-// console
-
-// 5. This API allows you to make only 3 requests per second. If you reload fast, you
-// will get this error with code 403. This is an error with the request. Remember,
-// fetch() does not reject the promise in this case. So create an error to reject
-// the promise yourself, with a meaningful error message
-
-// PART 2
-// 6. Now it's time to use the received data to render a country. So take the relevant
-// attribute from the geocoding API result, and plug it into the countries API that
-// we have been using.
-
-// 7. Render the country and catch any errors, just like we have done in the last
-// lecture (you can even copy this code, no need to type the same code)
-
-// Test data:
-// § Coordinates 1: 52.508, 13.381 (Latitude, Longitude)
-// § Coordinates 2: 19.037, 72.873
-// § Coordinates 3: -33.933, 18.474
-//////////////////////////////////////////////
-// SOLUTION
-const getInfoCountry = function (country) {
-    fetch(`https://restcountries.com/v2/alpha/${country}`)
-        .then((Response) => {
-            if (!Response.ok)
-                throw new Error(
-                    `unable to find country try again (${Response.status})`
-                );
-            return Response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            renderCountry(data);
-        });
+///////////////////////////////////////////////////////////////////
+const getLocation = function () {
+    return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 };
-const whereAmI = function (lat, lng) {
-    fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-        .then((Response) => {
-            console.log(Response);
-            if (!Response.ok)
-                throw new Error(
-                    `unable to find country try again (${Response.status})`
-                );
-            return Response.json();
-        })
-        .then((data) => {
-            console.log(data);
-            getInfoCountry(data.prov);
-        })
-        .catch((err) => {
-            console.error(`Something went wrong!!: ${err.message}`);
-            renderError(err.message);
-        })
-        .finally((countriesContainer.style.opacity = 1));
+const whereAmI = async function () {
+    // GET CURRENT USER LOCATION
+    try {
+        const resPos = await getLocation();
+        const { latitude: lat, longitude: lng } = resPos.coords;
+
+        // GET DATA FROM REVERSE GEO CODEING API
+        const resGeo = await fetch(
+            `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${lat}&lon=${lng}`
+        );
+        if (!resGeo.ok) throw new Error("not able to get location data");
+        const data = await resGeo.json();
+        const country = data.address.country_code;
+
+        // GET COUNTRY INFORMATION
+        const resInfo = await fetch(
+            `https://restcountries.com/v3.1/alpha/${country}`
+        ).finally((countriesContainer.style.opacity = 1));
+        if (!resInfo.ok) throw new Error("not able to get country info");
+        const resdata = await resInfo.json();
+        renderCountry(resdata[0]);
+    } catch (err) {
+        console.log(err.message);
+    }
 };
-btn.addEventListener("click", function () {
-    whereAmI(19.037, 72.873);
-});
-*/
-// console.log("test 1");
-// setTimeout(() => {
-//     console.log("0 sec timer");
-// }, 0);
-// Promise.resolve("promise 1").then((res) => console.log(res));
-// Promise.resolve("promise 2").then((res) => {
-//     // for (let i = 0; i < 10000000000; i++) {}
-//     console.log(res);
-// });
-// console.log("test 1 ended");
+whereAmI();
